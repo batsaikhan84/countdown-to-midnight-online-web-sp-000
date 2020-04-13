50324875fba4f67a8f4bfae4b8640da0a7fb8323
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-13 02:44:54 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,6 +8,11 @@ def countdown(number)
   return "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(integer)
-  sleep.(1)
+def countdown(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    sleep.
+  end
+  return "HAPPY NEW YEAR!"
 end
